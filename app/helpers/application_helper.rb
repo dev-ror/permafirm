c3043ff8f67508ceef71c52f2cd48633f6b138cb
usama akram <m.u.akram1997@gmail.com>
2020-06-03 20:15:23 -0400

    Login Sign Up

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+
+    def class_name
+        if controller.controller_name === 'sessions' || controller.controller_name === 'registrations'
+            return "login"
+        end
+
+        # case controller.controller_name # controller.controller_name is the variable we want to compare
+        # when 'sessions', 'registrations'
+        #     return "login"
+        # when 2
+        #   puts "it was 2"
+        # else
+        #   puts "it was something else"
+        # end
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_02_153537) do
+ActiveRecord::Schema.define(version: 2020_06_03_204253) do
 
   create_table "shops", force: :cascade do |t|
     t.string "shopify_domain", null: false
@@ -18,6 +18,18 @@ ActiveRecord::Schema.define(version: 2020_06_02_153537) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["shopify_domain"], name: "index_shops_on_shopify_domain", unique: true
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
