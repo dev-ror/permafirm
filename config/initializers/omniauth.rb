c646f24b7926817289a2abba15f819d06e523ab4
dev-junaid <junaid_hassan@hotmail.com>
2020-06-06 18:15:48 +0500

    fix shop

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 Rails.application.config.middleware.use(OmniAuth::Builder) do
 # frozen_string_literal: true
-
 provider :shopify,
   ShopifyApp.configuration.api_key,
   ShopifyApp.configuration.secret,
   scope: ShopifyApp.configuration.scope,
+  callback_path: '/app/auth/shopify/callback',
   setup: lambda { |env|
     strategy = env['omniauth.strategy']
 
