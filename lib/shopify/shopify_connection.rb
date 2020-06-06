6bca63aabca405633cf2d7965f143e806d4f578b
usama akram <m.u.akram1997@gmail.com>
2020-06-05 20:43:16 -0400

    Merged and added links

--- lib/shopify/shopify_connection.rb
+++ lib/shopify/shopify_connection.rb
@@ -1,12 +1,9 @@
 module Shopify::ShopifyConnection
     def self.connect_to_shop(domain, token)
-        byebug
         shopify_session = ShopifyAPI::Session.new(domain: domain,api_version: '2020-04',token: token)
         
         ShopifyAPI::Base.activate_session(shopify_session)
-        shop = ShopifyAPI::Shop.current
         
-        return shop
     end
     def self.location
         locations = ShopifyAPI::Location.find(:all)
